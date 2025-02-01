3cbe36bbb4fe40d87ce9a32489bbed3671cce258
XdillibX <rsnnrt13@gmail.com>
2025-02-01 13:29:52 +0900

    no message

--- exam/src/ex01/code/No3.java
+++ exam/src/ex01/code/No3.java
@@ -6,7 +6,7 @@ public class No3 {
     public static void main(String[] args) {
         int answer = (int)(Math.random() * 100) + 1;
         int myAnswer = 0;
-        int cnt = 0;
+        int count = 0;
 
         Scanner scanner;
 
@@ -14,7 +14,7 @@ public class No3 {
             System.out.print("1~100 사이의 수 입력 : ");
             scanner = new Scanner(System.in);
             myAnswer = scanner.nextInt();
-            cnt++;
+            count++;
 
             if (myAnswer > answer) {
                 System.out.println("더 작습니다");
@@ -24,7 +24,7 @@ public class No3 {
             }
             else {
                 System.out.print("정답입니다");
-                System.out.println("총 횟수 : " + cnt);
+                System.out.println("총 횟수 : " + count);
             }
         }
     }
