a51b8b4db184c52fd50a96eab48d3adc66f6957b
XdillibX <rsnnrt13@gmail.com>
2025-02-01 13:27:33 +0900

    no message

--- exam/src/ex01/code/No2.java
+++ exam/src/ex01/code/No2.java
@@ -4,22 +4,14 @@ public class No2 {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (String.valueOf(i).contains("3") || String.valueOf(i).contains("6") || String.valueOf(i).contains("9")) {
-                if (i % 10 == 0) {
-                    System.out.println("CLAP");
-                }
-                else {
-                    System.out.print("CLAP\t");
-                }
+                System.out.print("CLAP\t");
             }
             else {
-                if (i % 10 == 0) {
-                    System.out.println(i);
-                }
-                else {
-                    System.out.print(i + "\t\t");
-                }
+                System.out.print(i + "\t\t");
             }
-
+            if (i % 10 == 0) {
+                System.out.print("\n");
+            }
         }
     }
 }
