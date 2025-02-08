8f80ed0a0db5538ea85441aa4311957da6f6a72d
XdillibX <rsnnrt13@gmail.com>
2025-02-08 14:49:29 +0900

    no message

--- exam/src/ex01/code/No4/MyMethod.java
+++ exam/src/ex01/code/No4/MyMethod.java
@@ -1,7 +1,6 @@
 package ex01.code.No4;
 
 public class MyMethod {
-
     public int sum(int[] intArray) {
         int sum = 0;
         for (int i = 0; i < intArray.length; i++) {
@@ -9,13 +8,15 @@ public class MyMethod {
         }
         return sum;
     }
+
     public double average(int[] intArray) {
         int sum = 0;
         for (int i = 0; i < intArray.length; i++) {
             sum += intArray[i];
         }
-        return (float)sum / intArray.length;
+        return (double) sum / intArray.length;
     }
+
     public int max(int[] intArray) {
         int max = intArray[0];
         for (int i = 1; i < intArray.length; i++) {
@@ -25,6 +26,7 @@ public class MyMethod {
         }
         return max;
     }
+
     public int min(int[] intArray) {
         int min = intArray[0];
         for (int i = 1; i < intArray.length; i++) {
